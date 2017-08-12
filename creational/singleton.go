ed1e76ce68a69d99bda2b34f5714c7045fdfc300
Ben Wells <b.v.wells@gmail.com>
2017-08-12 19:41:45 +0100

    Update singleton.go

--- creational/singleton.go
+++ creational/singleton.go
@@ -1,1 +1,18 @@
 package creational
+
+import (
+    "sync"
+)
+
+type singleton struct {
+}
+
+var instance *singleton
+var once sync.Once
+
+func GetInstance() *singleton {
+    once.Do(func() {
+        instance = &singleton{}
+    })
+    return instance
+}
