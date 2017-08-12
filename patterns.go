86ac856970ae1662244a22854fe622edbb13247c
Ben Wells <b.v.wells@gmail.com>
2017-08-12 20:32:59 +0100

    Update patterns.go

--- patterns.go
+++ patterns.go
@@ -1,1 +1,4 @@
+// Package patterns is the root of the packages used to access go-patterns
+// examples. See https://godoc.org/github.com/bvwells/go-patterns for a full list
+// of sub-packages.
 package patterns
