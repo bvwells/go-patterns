54ef9b7f9f048b61bac8ec15f76d11ff8c432f4d
Ben Wells <b.v.wells@gmail.com>
2017-08-14 22:37:26 +0100

    Update singleton.go

--- creational/singleton.go
+++ creational/singleton.go
@@ -4,12 +4,17 @@ import (
     "sync"
 )
 
+// Singleton deacribes a struct of which only a single
+// instance can exist.
 type Singleton struct {
 }
 
 var instance *Singleton
 var once sync.Once
 
+// GetInstance ensures that only a single instance of the
+// struct singleton gets created and provides a global point
+// to access to it.
 func GetInstance() *Singleton {
     once.Do(func() {
         instance = &Singleton{}
