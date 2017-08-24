16825f061ffa4aa568a93ac13bdfb098ce15298b
Ben Wells <b.v.wells@gmail.com>
2017-08-24 21:48:08 +0100

    Add prototype design pattern

--- creational/prototype.go
+++ creational/prototype.go
@@ -1,1 +1,80 @@
 package creational
+
+import (
+	"bytes"
+	"fmt"
+)
+
+// INode a document object model node
+type INode interface {
+	// String returns the name of the node
+	String() string
+	// Parent returns the node parent
+	Parent() INode
+	// SetParent sets the node parent
+	SetParent(node INode)
+	// Children returns the node children nodes
+	Children() []INode
+	// AddChild adds a child node
+	AddChild(child INode)
+	// Clone clones a node
+	Clone() INode
+}
+
+// Node implements the INode interface
+type Node struct {
+	name     string
+	parent   INode
+	children []INode
+}
+
+// NewNode makes a new Node
+func NewNode(name string) *Node {
+	return &Node{
+		name:     name,
+		parent:   nil,
+		children: make([]INode, 0),
+	}
+}
+
+// Parent returns the INode parent
+func (n *Node) Parent() INode {
+	return n.parent
+}
+
+// SetParent sets the INode parent
+func (n *Node) SetParent(node INode) {
+	n.parent = node
+}
+
+// Children returns the INode children INodes
+func (n *Node) Children() []INode {
+	return n.children
+}
+
+// AddChild adds a child INode
+func (n *Node) AddChild(child INode) {
+	copy := child.Clone()
+	copy.SetParent(n)
+	n.children = append(n.children, copy)
+}
+
+// Clone makes a copy of particular INode. Note that the INode becomes a
+// root of new orphan tree
+func (n *Node) Clone() INode {
+	copy := NewNode(n.name)
+	for _, child := range n.children {
+		copy.AddChild(child)
+	}
+	return copy
+}
+
+// String returns string representation of INode
+func (n *Node) String() string {
+	buffer := bytes.NewBufferString(n.name)
+	for _, c := range n.Children() {
+		name := c.String()
+		fmt.Fprintf(buffer, "\n %s", name)
+	}
+	return buffer.String()
+}
--- creational/prototype_test.go
+++ creational/prototype_test.go
@@ -1,1 +1,57 @@
 package creational
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewNode_ReturnsNonNil(t *testing.T) {
+	node := NewNode("node")
+	assert.NotNil(t, node)
+}
+
+func TestNewNode_SetsName(t *testing.T) {
+	expectedName := "node"
+	actualName := NewNode(expectedName).name
+	assert.Equal(t, expectedName, actualName)
+}
+
+func TestSetParent_SetsParent(t *testing.T) {
+	parent := NewNode("parent")
+	child := NewNode("child")
+	child.SetParent(parent)
+	assert.Equal(t, parent, child.parent)
+}
+
+func TestParent_ReturnsParent(t *testing.T) {
+	parent := NewNode("parent")
+	child := NewNode("child")
+	child.parent = parent
+	assert.Equal(t, parent, child.Parent())
+}
+
+func TestAddChild_AddsChildren(t *testing.T) {
+	parent := NewNode("parent")
+	child1 := NewNode("child1")
+	parent.AddChild(child1)
+	child2 := NewNode("child2")
+	parent.AddChild(child2)
+
+	assert.Equal(t, 2, len(parent.children))
+	assert.Equal(t, child1.String(), parent.children[0].String())
+	assert.Equal(t, child2.String(), parent.children[1].String())
+}
+
+func TestClone_ClonesHierarchicalTree(t *testing.T) {
+	top := NewNode("parent")
+	hierarchy := []string{"level1", "level2", "level3"}
+	parent := top
+	for i := 0; i < len(hierarchy); i++ {
+		node := NewNode(hierarchy[i])
+		parent.AddChild(node)
+		parent = node
+	}
+	clone := top.Clone()
+	assert.Equal(t, top.String(), clone.String())
+}
