a44fcf5134070657da7bf19cbcbd92bca71a5430
Ben Wells <b.v.wells@gmail.com>
2017-09-10 14:48:38 +0100

    Add builder design pattern

--- creational/builder.go
+++ creational/builder.go
@@ -1,1 +1,55 @@
 package creational
+
+/*
+	Example of builder pattern:
+
+	builder := NewConcreteBuilder()
+	director := NewDirector(builder)
+	director.Construct()
+	product := builder.GetResult()
+*/
+
+// Director is the object which orchestrates the building of a product.
+type Director struct {
+	builder Builder
+}
+
+// NewDirector creates a new Director with a specified Builder.
+func NewDirector(builder Builder) Director {
+	return Director{builder}
+}
+
+// Construct builds the product from a series of steps.
+func (d *Director) Construct() {
+	d.builder.Build()
+}
+
+// Build is an interface for building.
+type Builder interface {
+	Build()
+}
+
+// ConcreteBuilder is a builder for building a Product
+type ConcreteBuilder struct {
+	built bool
+}
+
+// NewConcreteBuilder returns a new Builder.
+func NewConcreteBuilder() ConcreteBuilder {
+	return ConcreteBuilder{false}
+}
+
+// Build builds the product.
+func (b *ConcreteBuilder) Build() {
+	b.built = true
+}
+
+// GetResult returns the Product which has been build during the Build step.
+func (b *ConcreteBuilder) GetResult() Product {
+	return Product{b.built}
+}
+
+// Product describes the product to be built.
+type Product struct {
+	Built bool
+}
--- creational/builder_test.go
+++ creational/builder_test.go
@@ -1,1 +1,62 @@
 package creational
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewConcreteBuilder_ReturnsNonNil(t *testing.T) {
+	builder := NewConcreteBuilder()
+	assert.NotNil(t, builder)
+}
+
+func TestNewConcreteBuilder_SetsBuildToFalse(t *testing.T) {
+	builder := NewConcreteBuilder()
+	assert.False(t, builder.built)
+}
+
+func TestBuild_SetsBuildToTrue(t *testing.T) {
+	builder := NewConcreteBuilder()
+	builder.Build()
+	assert.True(t, builder.built)
+}
+
+func TestGetResult_WhenBuildNotCalled_ReturnsUnBuiltProduct(t *testing.T) {
+	builder := NewConcreteBuilder()
+	product := builder.GetResult()
+	assert.False(t, product.Built)
+}
+
+func TestGetResult_WhenBuildCalled_ReturnsBuiltProduct(t *testing.T) {
+	builder := NewConcreteBuilder()
+	builder.Build()
+	product := builder.GetResult()
+	assert.True(t, product.Built)
+}
+
+func TestNewDirector_ReturnsNonNil(t *testing.T) {
+	director := NewDirector(nil)
+	assert.NotNil(t, director)
+}
+
+func TestNewDirector_SetsBuilder(t *testing.T) {
+	builder := NewConcreteBuilder()
+	director := NewDirector(&builder)
+	assert.Equal(t, &builder, director.builder)
+}
+
+func TestConstruct_SetsBuiltOnBuilder(t *testing.T) {
+	builder := NewConcreteBuilder()
+	director := NewDirector(&builder)
+	director.Construct()
+	assert.True(t, builder.built)
+}
+
+func TestConstruct_BuildsProduct(t *testing.T) {
+	builder := NewConcreteBuilder()
+	director := NewDirector(&builder)
+	director.Construct()
+	product := builder.GetResult()
+	assert.True(t, product.Built)
+}
