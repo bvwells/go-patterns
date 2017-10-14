06e45afec1b5a7c310b18ade41291c72a195ad75
Ben Wells <b.v.wells@gmail.com>
2017-10-14 21:19:31 +0100

    Add some documentation

--- structural/decorator.go
+++ structural/decorator.go
@@ -4,8 +4,10 @@ import (
 	"log"
 )
 
+// TaskFunc represents a function to perform a task.
 type TaskFunc func(int) int
 
+// LogDecorate decorates a task functions execution with some logging.
 func LogDecorate(fn TaskFunc) TaskFunc {
 	return func(taskID int) int {
 		log.Printf("Starting task with ID %v.\n", taskID)
