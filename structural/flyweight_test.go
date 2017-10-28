8b8b279f6e2591a91c0b1f2b041cb684c411f010
Ben Wells <b.v.wells@gmail.com>
2017-10-28 20:50:06 +0100

    Implement flyweight design patter

---
+++ structural/flyweight_test.go
@@ -0,0 +1,40 @@
+package structural
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewFlyweight_ReturnsNonNil(t *testing.T) {
+	flyweight := NewFlyweight("McFly")
+	assert.NotNil(t, flyweight)
+}
+
+func TestNewFlyweight_SetsName(t *testing.T) {
+	name := "McFly"
+	flyweight := NewFlyweight(name)
+	assert.Equal(t, name, flyweight.Name)
+}
+
+func TestNewFlyweightFactory_ReturnsNonNil(t *testing.T) {
+	factory := NewFlyweightFactory()
+	assert.NotNil(t, factory)
+}
+
+func TestGetFlyweight_WhenFlyweightDoesNotExist_CreatesAndReturnsNewInstance(t *testing.T) {
+	factory := NewFlyweightFactory()
+	marty := factory.GetFlyweight("Marty McFly")
+	george := factory.GetFlyweight("George McFly")
+	assert.Len(t, factory.pool, 2)
+	assert.Equal(t, marty, factory.pool["Marty McFly"])
+	assert.Equal(t, george, factory.pool["George McFly"])
+}
+
+func TestGetFlyweight_WhenFlyweightAlreadyExists_ReturnsInstance(t *testing.T) {
+	factory := NewFlyweightFactory()
+	marty := factory.GetFlyweight("McFly")
+	george := factory.GetFlyweight("McFly")
+	assert.Equal(t, marty, george)
+	assert.Len(t, factory.pool, 1)
+}
