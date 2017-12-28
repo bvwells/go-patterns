e0023293c5a33612cea2e2066075841e35d830ce
Ben Wells <b.v.wells@gmail.com>
2017-12-28 20:54:20 +0000

    Implement iterator design pattern

--- behavioral/iterator.go
+++ behavioral/iterator.go
@@ -1,1 +1,56 @@
 package behavioral
+
+// Iterator is an interface for an iterator.
+type Iterator interface {
+
+	// Index returns the index of the current iterator.
+	Index() int
+
+	// Value returns the current value of the iterator.
+	Value() interface{}
+
+	// HasNext returns whether another next element exists.
+	HasNext() bool
+
+	// Next increments the iterator to point to the next element.
+	Next()
+
+	// End sets the iterator to point to the end element.
+	End()
+}
+
+// ArrayIterator is an iterator which iterates over an array.
+type ArrayIterator struct {
+	array []interface{}
+	index int
+}
+
+// Index returns the index of the current iterator.
+func (i *ArrayIterator) Index() int {
+	return i.index
+}
+
+// Value returns the current value of the iterator.
+func (i *ArrayIterator) Value() interface{} {
+	return i.array[i.index]
+}
+
+// HasNext returns whether another next element exists.
+func (i *ArrayIterator) HasNext() bool {
+	if i.index+1 == len(i.array) {
+		return false
+	}
+	return true
+}
+
+// Next increments the iterator to point to the next element.
+func (i *ArrayIterator) Next() {
+	if i.HasNext() {
+		i.index++
+	}
+}
+
+// End sets the iterator to point to the end element.
+func (i *ArrayIterator) End() {
+	i.index = len(i.array) - 1
+}
