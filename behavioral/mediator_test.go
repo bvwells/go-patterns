49ca2d556239b27f090cd9ff2116c69474131f9d
Ben Wells <b.v.wells@gmail.com>
2018-01-03 21:00:44 +0000

    Implement mediator design pattern

---
+++ behavioral/mediator_test.go
@@ -0,0 +1,22 @@
+package behavioral
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMediator(t *testing.T) {
+
+	bufferOutputWriter := outputWriter
+	outputWriter = new(bytes.Buffer)
+	defer func() { outputWriter = bufferOutputWriter }()
+
+	mediator := NewMediator()
+	mediator.Ted.Talk()
+
+	assert.Equal(t, "Ted: Bill?\n"+
+		"Bill: What?\n"+
+		"Ted: Strange things are afoot at the Circle K.\n", outputWriter.(*bytes.Buffer).String())
+}
